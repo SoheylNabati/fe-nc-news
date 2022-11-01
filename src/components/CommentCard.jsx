21b2f9a5d1627690cb09ff1ef1757f6dc1e8014a
soheyl <soheyl.nabati3@gmail.com>
2022-11-01 16:26:51 +0000

    added ticket 8 and some styling

---
+++ src/components/CommentCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Comments from "./style/Comments.css";
+
+export default function CommentCard({ comment, i }) {
+  const { author, votes, body, created_at } = comment;
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "numeric", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+  return (
+    <div className="single_comment">
+      <h4>
+        Comment no: {i + 1} By {author}:
+      </h4>
+      <h5>Date: {formatDate(created_at)}</h5>
+      <p className="text">{body}</p>
+      <h5>Votes: {votes}</h5>
+    </div>
+  );
+}
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getCommentByArticleID } from "../api";
+import CommentCard from "./CommentCard";
 
 export default function Comments() {
-  return <div>Comments</div>;
+  const [comments, setComments] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { article_id } = useParams();
+  useEffect(() => {
+    setLoading(true);
+    getCommentByArticleID(article_id).then(({ comments }) => {
+      setComments(comments);
+      setLoading(false);
+    });
+  }, [article_id]);
+  if (loading) return <h2>loading...</h2>;
+  return (
+    <div>
+      {comments.map((comment, i) => {
+        return <CommentCard key={comment.comment_id} comment={comment} i={i} />;
+      })}
+    </div>
+  );
 }
