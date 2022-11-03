07bb2476f890997593b32b55774f1e222f97297e
soheyl <soheyl.nabati3@gmail.com>
2022-11-03 11:05:46 +0000

    added sort by and order by

--- src/pages/ArticlesPage.jsx
+++ src/pages/ArticlesPage.jsx
@@ -1,19 +1,9 @@
-import { toHaveTextContent } from "@testing-library/jest-dom/dist/matchers";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getArticles } from "../api";
 import AllArticles from "../components/AllArticles";
 
 export default function ArticlesPage() {
-  const sortInputs = [
-    "created_at",
-    "votes",
-    "article_id",
-    "title",
-    "topic",
-    "author",
-    "body",
-  ];
   const [existingArticles, setArticles] = useState([]);
   const [order, setOrder] = useState("DESC");
   const [sort, setSort] = useState("created_at");
@@ -28,38 +18,35 @@ export default function ArticlesPage() {
     });
   }, [topic, sort, order]);
 
-  const handleOrder = () => {
-    if (order === "DESC") {
-      setOrder("ASC");
-    } else if (order === "ASC") {
-      setOrder("DESC");
-    }
-    // textContent = "descending";
+  const handleOrder = (e) => {
+    setOrder(e.target.value);
+  };
+  const handleSort = (e) => {
+    setSort(e.target.value);
   };
 
   if (loading) return <h2>loading...</h2>;
   return (
     <div>
-      <ul>
-        <h3>sort by: </h3>
-        {sortInputs.map((e, i) => (
-          <button
-            key={i}
-            onClick={() => {
-              setSort(e);
-            }}
-          >
-            {e}
-          </button>
-        ))}
-      </ul>
-      <h3>order</h3>
-      <input
-        value="ascending/descending"
-        id="btn"
-        type="button"
-        onClick={handleOrder}
-      ></input>
+      <h2>click on article to read more</h2>
+      <form>
+        <label htmlFor="sort_by">Sort By </label>
+        <select id="sort_by" onChange={handleSort}>
+          <option value="created_at">Date</option>
+          <option value="title">Title</option>
+          <option value="author">Author</option>
+          <option value="votes">Votes</option>
+          <option value="topic">Topic</option>
+          <option value="body">Body</option>
+          <option value="comment_count">Comment Count</option>
+        </select>
+        <label htmlFor="order">Order </label>
+        <select id="order" onChange={handleOrder}>
+          <option value="DESC">descending</option>
+          <option value="ASC">Ascending</option>
+        </select>
+        {/* <button type="submit">submit</button> */}
+      </form>
       <AllArticles existingArticles={existingArticles} />
     </div>
   );
